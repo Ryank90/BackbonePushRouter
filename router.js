8e6f4751a41e165e9e2510f030c41be6b8276715
Ryan Kerry <rkerry1@gmail.com>
2014-06-25 13:08:41 +0100

    Updated to check for history

--- router.js
+++ router.js
@@ -23,20 +23,23 @@ if (Backbone.history && Backbone.history._hasPushState) {
 
 // JQuery elements Process
 // =======================================================
-// check for anchors with data-override=true appended
-$(document).on('click', 'a:not([data-override])', function (e) {
-    // get the anchor
-    var href = $(this).attr('href');
-    // get the protocal
-    var protocal = this.protocal + '//';
-    
-    // make sure protocal is not part of the url we have got
-    if (href.slice(protocol.length) !== protocol) {
-        // prevent the default click path
-        e.preventDefault();
-        // fire the navigate process and trigger event
-        Backbone.history.navigate(href, {
-            trigger: true
-        });
-    } 
-});
+// check to see if we have backbone.history and pushstates
+if (Backbone.history && Backbone.history._hasPushState) {
+    // check for anchors with data-override=true appended
+    $(document).on('click', 'a:not([data-override])', function (e) {
+        // get the anchor
+        var href = $(this).attr('href');
+        // get the protocal
+        var protocal = this.protocal + '//';
+        
+        // make sure protocal is not part of the url we have got
+        if (href.slice(protocol.length) !== protocol) {
+            // prevent the default click path
+            e.preventDefault();
+            // fire the navigate process and trigger event
+            Backbone.history.navigate(href, {
+                trigger: true
+            });
+        } 
+    });
+}
